88e57fa71459b1c4e912c37ef9888f752e1d927d
sufuan <abusufian49539@gmail.com>
2025-08-07 12:31:25 +0600

    user

--- resources/js/Pages/User/Profile/Edit.jsx
+++ resources/js/Pages/User/Profile/Edit.jsx
@@ -1,6 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import DeleteUserForm from './Partials/DeleteUserForm';
+
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 
@@ -29,9 +29,7 @@ export default function Edit({ mustVerifyEmail, status }) {
                         <UpdatePasswordForm className="max-w-xl" />
                     </div>
 
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
+                   
                 </div>
             </div>
         </AuthenticatedLayout>
