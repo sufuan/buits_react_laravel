acbb6f7f4805bef4348e8ce16e3f4d8956b6385f
sufuan <abu49539@gmail.com>
2026-02-23 13:43:23 +0600

    cpanel test

---
+++ public/build/assets/index-CyJJblEx.js
@@ -0,0 +1,1 @@
+import{b as r,g as t}from"./app-Dy3Ft7RM.js";var a=r();const e=t(a);export{e as R,a as r};
---
+++ public/build/assets/label-dhblzcf9.js
@@ -0,0 +1,1 @@
+import{r as o,j as r}from"./app-Dy3Ft7RM.js";import{c as t}from"./index-CL1IFjib.js";const l=o.forwardRef(({className:e,...a},s)=>r.jsx("label",{ref:s,className:t("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",e),...a}));l.displayName="Label";export{l as L};
